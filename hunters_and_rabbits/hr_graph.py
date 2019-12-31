ee7247b9cf472787ad05508c602f3279a369e206
Krotera <01101011@tuta.io>
2019-12-30 20:36:03 -0500

    Refactor graph methods to work with ids

--- hunters_and_rabbits/hr_graph.py
+++ hunters_and_rabbits/hr_graph.py
@@ -4,7 +4,20 @@
 #
 # Contact: 01101011@tuta.io
 class Vertex:
-    """Vertex with an id and color field. Equality-checked and hashed by id."""
+    """Vertex with an id and color field.
+
+    Vertices are equality-checked and hashed by id alone.
+    E.g.,
+    v0 = Vertex("same_id", "black", foo, ...)
+    v1 = Vertex("same_id", "white", bar, ...)
+    v_list = [v0]
+
+    >>> v1 in v_list
+    True
+
+    >>> v0 == v1
+    True
+    """
     def __init__(self, id, color):
         self.id = id
         self.color = color
@@ -37,17 +50,6 @@ class Graph(dict):
     and edges in the graph, resp.
     For DENSE graphs (high edge:vertex ratio), an adjacency MATRIX graph's
     O(|V|^2) memory footprint is more efficient.
-
-    Sets (and dicts) are actually hash tables in Python, so to search, add, or
-    delete a vertex's edge is average O(1) and worst O(|E|) where |E| is number
-    of edges in the graph (which is the most edges a vertex can have).
-    Similarly, search and add operations on vertices in the graph are average
-    O(1) and worst O(|V|), but deleting a vertex, v, is average O(v°) where
-    v°, the degree of v, is the number of edges v has and worst O(|V|^2).
-    This is because deleting the vertex is worst |V|, then deleting an edge is
-    worst |E| for up to |E| edges, so we're left with |V|+|E|^2. Because we
-    use sets and can't have redundant edges, and because a vertex has at most
-    |V|+1 edges, |V|+|E|^2 becomes |V|+(|V|+1)^2 ≈ |V|^2.
     """
     __edge_count = 0
 
@@ -62,50 +64,73 @@ class Graph(dict):
     # Avg O(1), worst O(|E|)
     def add_vert(self, vert):
         """Add a vertex to the graph."""
-        if self.has_vert(vert):
+        if self.has_vert(vert.id):
             raise ValueError("Graph.add_vert(): Vertex already in graph!")
 
         self[vert] = set()
 
     # Avg O(1), worst O(|V|)
-    def add_edge(self, v0, v1):
-        """Add an edge between two vertices, v0 and v1, in the graph.
+    def add_edge(self, id0, id1):
+        """Add an edge between two vertices by their id (id0 and id1).
 
         The vertices must already be in the graph.
         """
-        if self.has_edge(v0, v1):
+        if self.has_edge(id0, id1):
             raise ValueError("Graph.add_edge(): Edge already in graph!")
-        if not ((self.has_vert(v0)) and
-            (self.has_vert(v1))):
+        if not ((self.has_vert(id0)) and
+            (self.has_vert(id1))):
             raise KeyError("Graph.add_edge(): One or both edge endpoints are" +\
                            " not vertices in the graph!")
+
+        v0 = self.get_vert(id0)
+        v1 = self.get_vert(id1)
 
         self[v0].add(v1)
         self[v1].add(v0)
         self.__edge_count += 1
 
     # Avg O(v°), worst O(|V|^2)
-    def del_vert(self, vert):
-        """Delete a vertex in the graph."""
-        if not self.has_vert(vert):
+    def del_vert(self, id):
+        """Delete a vertex in the graph by its id."""
+        if not self.has_vert(id):
             raise KeyError("Graph.del_vert(): Vertex not in graph!")
 
+        vert = self.get_vert(id)
         # Shallow copy adj list to avoid list change during iteration
-        neighbors_tmp = self[vert].copy()
+        neighbors_copy = self[vert].copy()
 
-        for n in neighbors_tmp:
+        for n in neighbors_copy:
             self.del_edge(vert, n)
 
         del self[vert]
 
     # Avg O(1), worst O(|E|)
     def del_edge(self, v0, v1):
-        """Delete an edge between two vertices, v0 and v1, in the graph."""
-        if not self.has_edge(v0, v1):
-            raise ValueError("Graph.del_edge(): Edge not in graph!")
+        """Delete an edge between two vertices, v0 and v1, in the graph.
 
-        self[v0].remove(v1)
-        self[v1].remove(v0)
+        v0 and v1 can either be Vertex objects or vertex ids.
+        When used with ids, the necessary calls to get_vert() incur an expensive
+        linear search.
+        """
+        # Vertices, as called by del_vert()
+        if ((type(v0) == Vertex) and
+            (type(v1) == Vertex)):
+            if not self.has_edge(v0.id, v1.id):
+                raise ValueError("Graph.del_edge(): Edge not in graph!")
+
+            self[v0].remove(v1)
+            self[v1].remove(v0)
+        # ids
+        else:
+            if not self.has_edge(v0, v1):
+                raise ValueError("Graph.del_edge(): Edge not in graph!")
+
+            vert0 = self.get_vert(v0)
+            vert1 = self.get_vert(v1)
+
+            self[vert0].remove(vert1)
+            self[vert1].remove(vert0)
+
         self.__edge_count -= 1
 
     def get_vert_count(self):
@@ -116,20 +141,35 @@ class Graph(dict):
         """Return the number of edges in the graph."""
         return self.__edge_count
 
-    def has_vert(self, vert):
-        """Return whether a vertex is in the graph."""
-        return vert in self
+    def has_vert(self, id):
+        """Return whether a vertex is in the graph by its id."""
+        # Graph is a dict, Python dicts are hash tables, and vertices are
+        # hashed by id, so we can do fast lookup with a dummy vertex possessing
+        # the same id.
+        return Vertex(id, "dummy") in self
 
-    def has_edge(self, v0, v1):
-        """Return whether two vertices, v0 and v1, share an edge."""
-        return ((self.has_vert(v0)) and
-            (self.has_vert(v1)) and
-            (v1 in self[v0]) and
-            (v0 in self[v1]))
+    def has_edge(self, id0, id1):
+        """Return whether two vertices by id (v0 and v1) share an edge."""
+        return ((self.has_vert(id0)) and
+            (self.has_vert(id1)) and
+            (Vertex(id1, "dummy") in self[Vertex(id0, "dummy")]) and
+            (Vertex(id0, "dummy") in self[Vertex(id1, "dummy")]))
 
-    def neighbors(self, vert):
-        """Return a vertex's neighbors as a set."""
-        if not self.has_vert(vert):
+    # Avg and worst O(|V|)
+    def get_vert(self, id):
+        """Return a vertex by its id."""
+        if not self.has_vert(id):
+            raise KeyError("Graph.get_vert(): Vertex not in graph!")
+
+        for k in self.keys():
+            if k.id == id:
+                return k
+        raise RuntimeError("Graph.get_vert(): Fatal disagreement with " +\
+                           "has_vert()! This must be a bug.")
+
+    def neighbors(self, id):
+        """Return a vertex's neighbors as a set. Vertex specified by its id."""
+        if not self.has_vert(id):
             raise KeyError("Graph.neighbors(): Vertex not in graph!")
 
-        return self[vert]
+        return self[Vertex(id, "dummy")]
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -23,27 +23,27 @@ class BasicGraph(unittest.TestCase):
         self.g.add_vert(self.v3)
         self.g.add_vert(self.v4)
 
-        self.g.add_edge(self.v1, self.v2)
-        self.g.add_edge(self.v1, self.v3)
-        self.g.add_edge(self.v2, self.v3)
-        self.g.add_edge(self.v3, self.v4)
+        self.g.add_edge(self.v1.id, self.v2.id)
+        self.g.add_edge(self.v1.id, self.v3.id)
+        self.g.add_edge(self.v2.id, self.v3.id)
+        self.g.add_edge(self.v3.id, self.v4.id)
 
         # Sanity check
         self.assertEqual(self.g.get_vert_count(), 4)
         self.assertEqual(self.g.get_edge_count(), 4)
 
-        self.assertTrue(self.g.has_vert(self.v1))
-        self.assertTrue(self.g.has_vert(self.v2))
-        self.assertTrue(self.g.has_vert(self.v3))
-        self.assertTrue(self.g.has_vert(self.v4))
+        self.assertTrue(self.g.has_vert(self.v1.id))
+        self.assertTrue(self.g.has_vert(self.v2.id))
+        self.assertTrue(self.g.has_vert(self.v3.id))
+        self.assertTrue(self.g.has_vert(self.v4.id))
 
-        self.assertTrue(self.g.has_edge(self.v1, self.v2))
-        self.assertTrue(self.g.has_edge(self.v1, self.v3))
-        self.assertTrue(self.g.has_edge(self.v2, self.v3))
-        self.assertTrue(self.g.has_edge(self.v3, self.v4))
-        self.assertFalse(self.g.has_edge(self.v2, self.v4))
+        self.assertTrue(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertTrue(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v3.id, self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v2.id, self.v4.id))
 
-        self.assertEqual(self.g.neighbors(self.v1), {self.v2, self.v3})
+        self.assertEqual(self.g.neighbors(self.v1.id), {self.v2, self.v3})
 
     def test_0_add_vert_normal(self):
         new_vert = Vertex(5,0)
@@ -51,8 +51,8 @@ class BasicGraph(unittest.TestCase):
         self.g.add_vert(new_vert)
 
         self.assertEqual(self.g.get_vert_count(), 5)
-        self.assertTrue(self.g.has_vert(new_vert))
-        self.assertEqual(self.g.neighbors(new_vert), set())
+        self.assertTrue(self.g.has_vert(new_vert.id))
+        self.assertEqual(self.g.neighbors(new_vert.id), set())
 
     def test_1_add_vert_already_in(self):
         v1_copy = Vertex(1,0)
@@ -72,181 +72,200 @@ class BasicGraph(unittest.TestCase):
         new_vert = Vertex(5,0)
 
         self.g.add_vert(new_vert)
-        self.g.add_edge(self.v4, new_vert)
+        self.g.add_edge(self.v4.id, new_vert.id)
 
         self.assertEqual(self.g.get_vert_count(), 5)
         self.assertEqual(self.g.get_edge_count(), 5)
-        self.assertTrue(self.g.has_vert(new_vert))
-        self.assertTrue(self.g.has_edge(new_vert, self.v4))
-        self.assertEqual(self.g.neighbors(new_vert),{self.v4})
+        self.assertTrue(self.g.has_vert(new_vert.id))
+        self.assertTrue(self.g.has_edge(new_vert.id, self.v4.id))
+        self.assertEqual(self.g.neighbors(new_vert.id), {self.v4})
 
-        self.g.add_edge(self.v4, self.v1)
+        self.g.add_edge(self.v4.id, self.v1.id)
 
         self.assertEqual(self.g.get_edge_count(), 6)
-        self.assertTrue(self.g.has_edge(self.v1, self.v4))
-        self.assertEqual(self.g.neighbors(self.v4),{new_vert, self.v1, self.v3})
+        self.assertTrue(self.g.has_edge(self.v1.id, self.v4.id))
+        self.assertEqual(self.g.neighbors(self.v4.id), {new_vert, self.v1, self.v3})
+
+        # Edge to self
+        self.g.add_edge(new_vert.id, new_vert.id)
+
+        self.assertEqual(self.g.get_edge_count(), 7)
+        self.assertTrue(self.g.has_edge(new_vert.id, new_vert.id))
+        self.assertEqual(self.g.neighbors(new_vert.id), {new_vert, self.v4})
 
     def test_3_add_edge_already_in(self):
         with self.assertRaises(ValueError):
-            self.g.add_edge(self.v3, self.v2)
+            self.g.add_edge(self.v3.id, self.v2.id)
 
         self.assertEqual(self.g.get_edge_count(), 4)
+
+        # Edge to self
+        new_vert = Vertex(5,0)
+
+        self.g.add_vert(new_vert)
+        self.g.add_edge(new_vert.id, new_vert.id)
+
+        with self.assertRaises(ValueError):
+            self.g.add_edge(new_vert.id, new_vert.id)
+
+        self.assertEqual(self.g.get_edge_count(), 5)
+        self.assertTrue(self.g.has_edge(new_vert.id, new_vert.id))
 
     def test_4_add_edge_missing_vert(self):
         new_vert = Vertex(5,0)
 
         with self.assertRaises(KeyError):
-            self.g.add_edge(self.v1, new_vert)
+            self.g.add_edge(self.v1.id, new_vert.id)
 
         self.assertEqual(self.g.get_edge_count(), 4)
-        self.assertFalse(self.g.has_edge(self.v1, new_vert))
+        self.assertFalse(self.g.has_edge(self.v1.id, new_vert.id))
 
     def test_5_add_edge_missing_verts(self):
         new_vert_1 = Vertex(5,0)
         new_vert_2 = Vertex(6,0)
 
         with self.assertRaises(KeyError):
-            self.g.add_edge(new_vert_1, new_vert_2)
+            self.g.add_edge(new_vert_1.id, new_vert_2.id)
 
         self.assertEqual(self.g.get_edge_count(), 4)
-        self.assertFalse(self.g.has_edge(new_vert_1, new_vert_2))
+        self.assertFalse(self.g.has_edge(new_vert_1.id, new_vert_2.id))
 
     def test_6_del_vert(self):
-        self.g.del_vert(self.v1)
+        self.g.del_vert(self.v1.id)
 
-        self.assertFalse(self.g.has_vert(self.v1))
-        self.assertTrue(self.g.has_vert(self.v2))
-        self.assertTrue(self.g.has_vert(self.v3))
-        self.assertTrue(self.g.has_vert(self.v4))
-        self.assertFalse(self.g.has_edge(self.v1, self.v2))
-        self.assertFalse(self.g.has_edge(self.v1, self.v3))
-        self.assertTrue(self.g.has_edge(self.v2, self.v3))
-        self.assertTrue(self.g.has_edge(self.v3, self.v4))
+        self.assertFalse(self.g.has_vert(self.v1.id))
+        self.assertTrue(self.g.has_vert(self.v2.id))
+        self.assertTrue(self.g.has_vert(self.v3.id))
+        self.assertTrue(self.g.has_vert(self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v3.id, self.v4.id))
         self.assertEqual(self.g.get_vert_count(), 3)
         self.assertEqual(self.g.get_edge_count(), 2)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v1)
-        self.assertEqual(self.g.neighbors(self.v2), {self.v3})
-        self.assertEqual(self.g.neighbors(self.v3), {self.v2, self.v4})
-        self.assertEqual(self.g.neighbors(self.v4), {self.v3})
+            self.g.neighbors(self.v1.id)
+        self.assertEqual(self.g.neighbors(self.v2.id), {self.v3})
+        self.assertEqual(self.g.neighbors(self.v3.id), {self.v2, self.v4})
+        self.assertEqual(self.g.neighbors(self.v4.id), {self.v3})
 
         v2_copy = Vertex(2,1) # Same id, different color
 
-        self.g.del_vert(v2_copy)
+        self.g.del_vert(v2_copy.id)
 
-        self.assertFalse(self.g.has_vert(self.v1))
-        self.assertFalse(self.g.has_vert(self.v2))
-        self.assertTrue(self.g.has_vert(self.v3))
-        self.assertTrue(self.g.has_vert(self.v4))
-        self.assertFalse(self.g.has_edge(self.v1, self.v2))
-        self.assertFalse(self.g.has_edge(self.v1, self.v3))
-        self.assertFalse(self.g.has_edge(self.v2, self.v3))
-        self.assertTrue(self.g.has_edge(self.v3, self.v4))
+        self.assertFalse(self.g.has_vert(self.v1.id))
+        self.assertFalse(self.g.has_vert(self.v2.id))
+        self.assertTrue(self.g.has_vert(self.v3.id))
+        self.assertTrue(self.g.has_vert(self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertFalse(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v3.id, self.v4.id))
         self.assertEqual(self.g.get_vert_count(), 2)
         self.assertEqual(self.g.get_edge_count(), 1)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v1)
+            self.g.neighbors(self.v1.id)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v2)
-        self.assertEqual(self.g.neighbors(self.v3), {self.v4})
-        self.assertEqual(self.g.neighbors(self.v4), {self.v3})
+            self.g.neighbors(self.v2.id)
+        self.assertEqual(self.g.neighbors(self.v3.id), {self.v4})
+        self.assertEqual(self.g.neighbors(self.v4.id), {self.v3})
 
     def test_7_del_vert_missing(self):
         new_vert = Vertex(5,0)
 
         with self.assertRaises(KeyError):
-            self.g.del_vert(new_vert)
+            self.g.del_vert(new_vert.id)
 
         self.assertEqual(self.g.get_vert_count(), 4)
         self.assertEqual(self.g.get_edge_count(), 4)
 
     def test_8_del_edge(self):
-        self.g.del_edge(self.v1, self.v2)
+        self.g.del_edge(self.v1.id, self.v2.id)
 
-        self.assertTrue(self.g.has_vert(self.v1))
-        self.assertTrue(self.g.has_vert(self.v2))
-        self.assertTrue(self.g.has_vert(self.v3))
-        self.assertTrue(self.g.has_vert(self.v4))
-        self.assertFalse(self.g.has_edge(self.v1, self.v2))
-        self.assertTrue(self.g.has_edge(self.v1, self.v3))
-        self.assertTrue(self.g.has_edge(self.v2, self.v3))
-        self.assertTrue(self.g.has_edge(self.v3, self.v4))
+        self.assertTrue(self.g.has_vert(self.v1.id))
+        self.assertTrue(self.g.has_vert(self.v2.id))
+        self.assertTrue(self.g.has_vert(self.v3.id))
+        self.assertTrue(self.g.has_vert(self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertTrue(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v3.id, self.v4.id))
         self.assertEqual(self.g.get_vert_count(), 4)
         self.assertEqual(self.g.get_edge_count(), 3)
-        self.assertEqual(self.g.neighbors(self.v1), {self.v3})
-        self.assertEqual(self.g.neighbors(self.v2), {self.v3})
-        self.assertEqual(self.g.neighbors(self.v3), {self.v4, self.v1, self.v2})
-        self.assertEqual(self.g.neighbors(self.v4), {self.v3})
+        self.assertEqual(self.g.neighbors(self.v1.id), {self.v3})
+        self.assertEqual(self.g.neighbors(self.v2.id), {self.v3})
+        self.assertEqual(self.g.neighbors(self.v3.id), {self.v4, self.v1, self.v2})
+        self.assertEqual(self.g.neighbors(self.v4.id), {self.v3})
 
-        self.g.del_edge(self.v3, self.v1) # Isolate v1
+        self.g.del_edge(self.v3.id, self.v1.id) # Isolate v1
 
-        self.assertTrue(self.g.has_vert(self.v1))
-        self.assertTrue(self.g.has_vert(self.v2))
-        self.assertTrue(self.g.has_vert(self.v3))
-        self.assertTrue(self.g.has_vert(self.v4))
-        self.assertFalse(self.g.has_edge(self.v1, self.v2))
-        self.assertFalse(self.g.has_edge(self.v1, self.v3))
-        self.assertTrue(self.g.has_edge(self.v2, self.v3))
-        self.assertTrue(self.g.has_edge(self.v3, self.v4))
+        self.assertTrue(self.g.has_vert(self.v1.id))
+        self.assertTrue(self.g.has_vert(self.v2.id))
+        self.assertTrue(self.g.has_vert(self.v3.id))
+        self.assertTrue(self.g.has_vert(self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertTrue(self.g.has_edge(self.v3.id, self.v4.id))
         self.assertEqual(self.g.get_vert_count(), 4)
         self.assertEqual(self.g.get_edge_count(), 2)
-        self.assertEqual(self.g.neighbors(self.v1), set())
-        self.assertEqual(self.g.neighbors(self.v2), {self.v3})
-        self.assertEqual(self.g.neighbors(self.v3), {self.v4, self.v2})
-        self.assertEqual(self.g.neighbors(self.v4), {self.v3})
+        self.assertEqual(self.g.neighbors(self.v1.id), set())
+        self.assertEqual(self.g.neighbors(self.v2.id), {self.v3})
+        self.assertEqual(self.g.neighbors(self.v3.id), {self.v4, self.v2})
+        self.assertEqual(self.g.neighbors(self.v4.id), {self.v3})
 
     def test_9_del_edge_missing(self):
         with self.assertRaises(ValueError):
-            self.g.del_edge(self.v1, self.v4)
+            self.g.del_edge(self.v1.id, self.v4.id)
 
         self.assertEqual(self.g.get_vert_count(), 4)
         self.assertEqual(self.g.get_edge_count(), 4)
 
     def test_10_del_all_vertices(self):
-        self.g.del_vert(self.v1)
-        self.g.del_vert(self.v2)
-        self.g.del_vert(self.v3)
-        self.g.del_vert(self.v4)
+        self.g.del_vert(self.v1.id)
+        self.g.del_vert(self.v2.id)
+        self.g.del_vert(self.v3.id)
+        self.g.del_vert(self.v4.id)
 
-        self.assertFalse(self.g.has_vert(self.v1))
-        self.assertFalse(self.g.has_vert(self.v2))
-        self.assertFalse(self.g.has_vert(self.v3))
-        self.assertFalse(self.g.has_vert(self.v4))
-        self.assertFalse(self.g.has_edge(self.v1, self.v2))
-        self.assertFalse(self.g.has_edge(self.v1, self.v3))
-        self.assertFalse(self.g.has_edge(self.v2, self.v3))
-        self.assertFalse(self.g.has_edge(self.v3, self.v4))
+        self.assertFalse(self.g.has_vert(self.v1.id))
+        self.assertFalse(self.g.has_vert(self.v2.id))
+        self.assertFalse(self.g.has_vert(self.v3.id))
+        self.assertFalse(self.g.has_vert(self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertFalse(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertFalse(self.g.has_edge(self.v3.id, self.v4.id))
         self.assertEqual(self.g.get_vert_count(), 0)
         self.assertEqual(self.g.get_edge_count(), 0)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v1)
+            self.g.neighbors(self.v1.id)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v2)
+            self.g.neighbors(self.v2.id)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v3)
+            self.g.neighbors(self.v3.id)
         with self.assertRaises(KeyError):
-            self.g.neighbors(self.v4)
+            self.g.neighbors(self.v4.id)
 
     def test_13_del_all_edges(self):
-        self.g.del_edge(self.v1, self.v2)
-        self.g.del_edge(self.v1, self.v3)
-        self.g.del_edge(self.v2, self.v3)
-        self.g.del_edge(self.v3, self.v4)
+        self.g.del_edge(self.v1.id, self.v2.id)
+        self.g.del_edge(self.v1.id, self.v3.id)
+        self.g.del_edge(self.v2.id, self.v3.id)
+        self.g.del_edge(self.v3.id, self.v4.id)
 
-        self.assertTrue(self.g.has_vert(self.v1))
-        self.assertTrue(self.g.has_vert(self.v2))
-        self.assertTrue(self.g.has_vert(self.v3))
-        self.assertTrue(self.g.has_vert(self.v4))
-        self.assertFalse(self.g.has_edge(self.v1, self.v2))
-        self.assertFalse(self.g.has_edge(self.v1, self.v3))
-        self.assertFalse(self.g.has_edge(self.v2, self.v3))
-        self.assertFalse(self.g.has_edge(self.v3, self.v4))
+        self.assertTrue(self.g.has_vert(self.v1.id))
+        self.assertTrue(self.g.has_vert(self.v2.id))
+        self.assertTrue(self.g.has_vert(self.v3.id))
+        self.assertTrue(self.g.has_vert(self.v4.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v2.id))
+        self.assertFalse(self.g.has_edge(self.v1.id, self.v3.id))
+        self.assertFalse(self.g.has_edge(self.v2.id, self.v3.id))
+        self.assertFalse(self.g.has_edge(self.v3.id, self.v4.id))
         self.assertEqual(self.g.get_vert_count(), 4)
         self.assertEqual(self.g.get_edge_count(), 0)
-        self.assertEqual(self.g.neighbors(self.v1), set())
-        self.assertEqual(self.g.neighbors(self.v2), set())
-        self.assertEqual(self.g.neighbors(self.v3), set())
-        self.assertEqual(self.g.neighbors(self.v4), set())
+        self.assertEqual(self.g.neighbors(self.v1.id), set())
+        self.assertEqual(self.g.neighbors(self.v2.id), set())
+        self.assertEqual(self.g.neighbors(self.v3.id), set())
+        self.assertEqual(self.g.neighbors(self.v4.id), set())
 
     def test_16_neighbors(self):
         v5 = Vertex(5,0)
@@ -254,13 +273,13 @@ class BasicGraph(unittest.TestCase):
 
         self.g.add_vert(v5)
 
-        self.assertEqual(self.g.neighbors(v5), set())
+        self.assertEqual(self.g.neighbors(v5.id), set())
 
         self.g.add_vert(v6)
-        self.g.add_edge(v5, v6)
+        self.g.add_edge(v5.id, v6.id)
 
-        self.assertEqual(self.g.neighbors(v5), {v6})
-        self.assertEqual(self.g.neighbors(v6), {v5})
+        self.assertEqual(self.g.neighbors(v5.id), {v6})
+        self.assertEqual(self.g.neighbors(v6.id), {v5})
 
 if __name__ == "__main__":
     unittest.main()
