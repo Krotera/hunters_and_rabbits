9f1b9022679a0f9d4792c9b7f8e717848f04d7d2
Krotera <01101011@tuta.io>
2020-01-26 20:58:26 -0500

    Add GitHub URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Dash implementation of the Hunters and Rabbits graph game",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Krotera/hunters_and_rabbits",
     packages=setuptools.find_packages(),
     install_requires=[
         "networkx>=2.4",
