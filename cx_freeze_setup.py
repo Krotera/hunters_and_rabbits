90d9330de9ddbc6a039f39b4e8aa396cc0468855
Krotera <01101011@tuta.io>
2020-01-29 17:47:10 -0500

    Add cx_Freeze setup script

---
+++ cx_freeze_setup.py
@@ -0,0 +1,45 @@
+import sys
+from cx_Freeze import setup, Executable
+sys.path.insert(0, "hunters_and_rabbits")
+
+"""Alternate setup file for freezing to EXE via cx_Freeze"""
+
+build_exe_options = {"packages": ["jinja2.ext"], "excludes": []}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="hunters_and_rabbits",
+    version="1.1.0",
+    install_requires=[
+        "networkx>=2.4",
+        "flask>=1.1.1",
+        "plotly>=4.4.1",
+        "dash>=1.7.0",
+        "dash-core-components>=1.6.0",
+        "dash-html-components>=1.0.2",
+        "numpy>=1.18.0",
+    ],
+    python_requires='>=3.8',
+
+    options = {"build_exe": build_exe_options},
+    executables = [
+        Executable("hunters_and_rabbits/hunters_and_rabbits.py", base=base)
+    ],
+
+    # PyPI metadata
+    author="Krotera",
+    author_email="01101011@tuta.io",
+    description="A Dash implementation of the Hunters and Rabbits graph game",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Krotera/hunters_and_rabbits",
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    "Operating System :: OS Independent",
+    ]
+)
