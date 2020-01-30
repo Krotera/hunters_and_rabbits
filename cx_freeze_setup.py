92f48f45556fb9c3fc6962b06eb102c15f13f3d6
Krotera <01101011@tuta.io>
2020-01-29 20:29:50 -0500

    Add license header to setup files

--- cx_freeze_setup.py
+++ cx_freeze_setup.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Contact: 01101011@tuta.io
 import sys
 from cx_Freeze import setup, Executable
 sys.path.insert(0, "hunters_and_rabbits")
